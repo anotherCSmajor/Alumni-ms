928b20ab39b10b1cb00f3ee89ebc5ade38e8209d
Saad Halim <saadhalim3d@gmail.com>
2025-09-16 23:47:19 +0530

    again trying

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+
 export default defineConfig({
   base: "/Alumni-ms", // Change this to your GitHub repository name
   plugins: [react(), tailwindcss()],
